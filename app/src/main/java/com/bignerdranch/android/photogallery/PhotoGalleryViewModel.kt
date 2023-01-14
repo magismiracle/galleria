80e01f8b92e163848d3a3c1733067a9d49e1e8af
anuradha <kje4@list.ru>
2023-01-14 09:26:33 +0700

    Initional commit

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryViewModel.kt
@@ -0,0 +1,33 @@
+package com.bignerdranch.android.photogallery
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+
+class PhotoGalleryViewModel(private val app:Application) : AndroidViewModel(app) {
+
+    var galleryItemLiveData: LiveData<List<GalleryItem>> = FlickrFetchr().fetchPhotos()
+    private val flickrFetchr = FlickrFetchr()
+    private val mutableSearchTerm = MutableLiveData<String>()
+    val searchTerm: String get() = mutableSearchTerm.value ?: ""
+
+    init {
+        mutableSearchTerm.value = QueryPreferences.getStoredQuery(app)
+
+        galleryItemLiveData = Transformations.switchMap(mutableSearchTerm) {
+        searchTerm ->
+        if (searchTerm.isBlank()) {
+            flickrFetchr.fetchPhotos()
+        }
+        else {
+            flickrFetchr.searchPhotos(searchTerm)
+        }
+        }
+    }
+    fun fetchPhotos(query: String= ""){
+        QueryPreferences.setStoredQuery(app,query)
+        mutableSearchTerm.value = query
+    }
+}
